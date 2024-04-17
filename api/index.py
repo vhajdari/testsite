a74b3d6684779d966f0d8e9acb0acf4dab496d2a
Veton Hajdari <vhajdari@users.noreply.github.com>
2024-04-17 16:39:23 -0500

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,11 @@
+from http.server import BaseHTTPRequestHandler
+import time
+ 
+class handler(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'+time().encode('utf-8'))
+        return
